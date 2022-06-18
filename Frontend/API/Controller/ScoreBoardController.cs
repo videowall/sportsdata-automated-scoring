f79f92f8dd3577d5d371665b2f78fcbcedfdf598
Marvin Auf der Landwehr <marvin.auf-der-landwehr@outlook.com>
2022-06-18 16:30:44 +0200

    Created Controller

---
+++ Frontend/API/Controller/ScoreBoardController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WBH.Livescoring.Frontend.Logic.ScoreBoard;
+
+namespace WBH.Livescoring.Frontend.API.Controller;
+
+/// <summary>
+/// Controller für die Spielstandsanzeige
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class ScoreBoardController : ControllerBase
+{
+    #region Fields
+
+    private readonly IScoreBoardProvider _provider;
+
+    #endregion
+
+    #region Constructors
+
+    public ScoreBoardController(IScoreBoardProvider provider)
+    {
+        _provider = provider;
+    }
+
+    #endregion
+    
+    #region Endpoints
+
+    /// <summary>
+    ///     Test Funktion
+    /// </summary>
+    /// <remarks>Testet die API</remarks>
+    /// <returns>Test</returns>
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("Test");
+    }
+
+    #endregion
+}
