fb3cec8cd5c45ce87a7da66a78e08203ce586cc9
Marvin Auf der Landwehr <marvin.auf-der-landwehr@outlook.com>
2022-06-18 16:50:13 +0200

    ScoreBoardInfo Selecting

--- Frontend/Logic/ScoreBoard/ScoreBoardProvider.cs
+++ Frontend/Logic/ScoreBoard/ScoreBoardProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using WBH.Livescoring.Frontend.DataAccessLayer.Primitives;
 using WBH.Livescoring.SportRadar;
 
 namespace WBH.Livescoring.Frontend.Logic.ScoreBoard;
@@ -8,14 +10,16 @@ internal sealed class ScoreBoardProvider : IScoreBoardProvider
     #region Fields
 
     private readonly ILiveScoutService _liveScoutService;
+    private readonly IContext _context;
 
     #endregion
 
     #region Constructors
 
-    public ScoreBoardProvider(ILiveScoutService liveScoutService)
+    public ScoreBoardProvider(ILiveScoutService liveScoutService, IContext context)
     {
         _liveScoutService = liveScoutService;
+        _context = context;
     }
 
     #endregion
@@ -24,7 +28,10 @@ internal sealed class ScoreBoardProvider : IScoreBoardProvider
 
     public ScoreBoardInfo GetScoreBoardInfo(long matchId)
     {
-        throw new NotImplementedException();
+        return _context.Query<Entities.Match>()
+            .Include(e => e.Scores)
+            .Select(ScoreBoardInfo.Project)
+            .FirstOrDefault();
     }
 
     public void BookMatch(long matchId)
