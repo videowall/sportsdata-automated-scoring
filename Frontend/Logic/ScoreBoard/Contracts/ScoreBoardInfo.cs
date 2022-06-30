962f92b5b6ba810ba6a4d689004de5d4fbdf0e8a
Marvin Auf der Landwehr <marvin.auf-der-landwehr@outlook.com>
2022-06-30 09:06:05 +0200

    Select Bug Fix

--- Frontend/Logic/ScoreBoard/Contracts/ScoreBoardInfo.cs
+++ Frontend/Logic/ScoreBoard/Contracts/ScoreBoardInfo.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-
-namespace WBH.Livescoring.Frontend.Logic.ScoreBoard;
+﻿namespace WBH.Livescoring.Frontend.Logic.ScoreBoard;
 
 public class ScoreBoardInfo
 {
@@ -27,36 +23,6 @@ public class ScoreBoardInfo
     public string ServiceHome {get;set;}
     public string ServiceAway {get;set;}
     public string MatchTime {get;set;}
-
-    #endregion
-
-    #region Projections
-
-    internal static Expression<Func<Entities.Match, ScoreBoardInfo>> Project()
-    {
-        return x => new ScoreBoardInfo
-        {
-            MatchTime = x.MatchTime,
-            Team1Line1 = x.Team1Line1,
-            Team1Line2 = x.Team1Line2,
-            Team2Line1 = x.Team2Line1,
-            Team2Line2 = x.Team2Line2,
-            Team1Set1 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set1).Sum(s => s.Team1).ToString("0"),
-            Team2Set1 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set1).Sum(s => s.Team2).ToString("0"),
-            Team1Set2 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set2).Sum(s => s.Team1).ToString("0"),
-            Team2Set2 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set2).Sum(s => s.Team2).ToString("0"),
-            Team1Set3 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set3).Sum(s => s.Team1).ToString("0"),
-            Team2Set3 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set3).Sum(s => s.Team2).ToString("0"),
-            Team1Set4 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set4).Sum(s => s.Team1).ToString("0"),
-            Team2Set4 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set4).Sum(s => s.Team2).ToString("0"),
-            Team1Set5 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set5).Sum(s => s.Team1).ToString("0"),
-            Team2Set5 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Set5).Sum(s => s.Team2).ToString("0"),
-            PointsTeam1 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Game).Sum(s => s.Team1).ToString("0"),
-            PointsTeam2 = x.Scores.AsQueryable().Where(s => s.Type == Entities.ScoreType.Game).Sum(s => s.Team2).ToString("0"),
-            ServiceHome = x.Service == Entities.HomeAway.Home ? "PFAD" : "",
-            ServiceAway = x.Service == Entities.HomeAway.Away ? "PFAD" : ""
-        };
-    }
 
     #endregion
 }
