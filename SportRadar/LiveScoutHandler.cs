0213b734a7bb602fdee156b565b4604757483bf2
Marvin Auf der Landwehr <marvin.auf-der-landwehr@outlook.com>
2022-07-08 09:59:25 +0200

    Saving all Tennis events

--- SportRadar/LiveScoutHandler.cs
+++ SportRadar/LiveScoutHandler.cs
@@ -101,7 +101,7 @@ internal sealed class LiveScoutHandler
     public async Task MatchListHandler(object sender, MatchListEventArgs e)
     {
         var matches = e.MatchList?
-            .Where(m => m.Category.Name.International.StartsWith("ITF", StringComparison.InvariantCultureIgnoreCase))
+            .Where(m => m.Sport.Name.International.Equals("Tennis", StringComparison.InvariantCultureIgnoreCase))
             .Select(item => new MatchListItem
             {
                 MatchId = item.MatchHeader.MatchId,
